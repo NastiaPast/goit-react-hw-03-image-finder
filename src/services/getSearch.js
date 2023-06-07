3ed4204db7d88a6f560ff6da649721306e61c58d
Anastasiia Pastukhova <nastapastuhova778@gmail.com>
2023-06-07 13:40:17 +0300

    changes

--- src/services/getSearch.js
+++ src/services/getSearch.js
@@ -1,22 +1,5 @@
-// const API_KEY = '35020941-f87e175105e6d04b9453ce51c';
-// const BASE_URL = 'https://pixabay.com/api';
-
-// const getSearch = (searchText, page) => {
-//   const params = new URLSearchParams({
-//     q: searchText,
-//     page: page,
-//     key: API_KEY,
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     per_page: 12,
-//   });
-
-//   return fetch(`${BASE_URL}?${params}`);
-// };
-// export default getSearch;
-const API_KEY = '35020941-f87e175105e6d04b9453ce51c'; 
+const API_KEY = '35020941-f87e175105e6d04b9453ce51c';
 const BASE_URL = 'https://pixabay.com/api/';
-const PICS_ON_PAGE = 12;
 
 export const getSearch = (searchText, page) => {
   const params = new URLSearchParams({
@@ -25,7 +8,7 @@ export const getSearch = (searchText, page) => {
     key: API_KEY,
     image_type: 'photo',
     orientation: 'horizontal',
-    per_page: PICS_ON_PAGE,
+    per_page: 12,
   });
 
   return fetch(`${BASE_URL}?${params}`);
